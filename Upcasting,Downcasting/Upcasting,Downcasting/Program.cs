e1b1870999ff887c347a6b500c85f78fa529c9cb
Azer444 <Azerrh@code.edu.az>
2022-07-06 02:08:20 +0400

    Upcasting,Downcasting

---
+++ Upcasting,Downcasting/Upcasting,Downcasting/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Upcasting_Downcasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Params
+
+
+            //    GetElements(1, 3, 5, 7, 8, 15, 21);
+            //}
+            //public static void GetElements(params int [] nums)
+            //{
+            //    foreach (var item in nums)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            #endregion
+            #region Enam
+            //CheckRole(1);
+            //static void CheckRole(int roleId)
+            //{
+            //    switch (roleId)
+            //    {
+            //        case (int)Roles.SuperAdmin:
+            //            Console.WriteLine("SuperAdmin");
+            //            break;
+            //        case (int)Roles.Admin:
+            //            Console.WriteLine("Admin");
+            //            break;
+            //        case (int)Roles.Member:
+            //            Console.WriteLine("Member");
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //}
+            #endregion
+            #region Nullable
+            //string name = null;
+            //int? age = null;
+            //var result == null? 55:60
+            //var result = age ?? 60;
+            //Console.WriteLine(result);
+            //Test test = null;
+            //Console.WriteLine(test.word.ToString());
+            #endregion
+
+
+            //Test1 test = new Test1("Orxan");
+            //Console.WriteLine(test.Num);
+            //Test2 test2 = new Test2();
+        }
+
+            #region Struck
+        //}
+        //struct Test1
+        //{
+        //    public int Num { get; set; }
+        //    public Test1(string name)
+        //    {
+        //        Num = 66;
+        //        Console.WriteLine(Num);
+        //    }
+
+        //}
+        //class Test2
+        //{
+        //    public int MyProperty { get; set; } = 66;
+        //    public Test2()
+        //    {
+        //        Console.WriteLine("Bosh");
+        //    }
+        //    public Test2(int b)
+        //    {
+        //        Console.WriteLine("Dolu");
+        //    }
+        //}
+
+        #endregion
+
+
+
+
+
+
+           #region Enam
+        //public enum Roles
+        //{
+        //     SuperAdmin,
+        //     Admin,
+        //     Member
+        //}
+        #endregion
+           #region Nullable
+        //class Test
+        //{
+        //    #region Struck
+        //    public int? number { get; set; }
+        //    public string word { get; set; }
+        //    #endregion
+
+        //}
+        #endregion
+
+    }
